487b1363909ff2abc74a676c6cdbe8239b1fa8c4
Stephen <liwp.stephen@gmail.com>
2016-03-03 08:55:21 +0800

    add display and format test code

--- gopl.io/ch12/display.go
+++ gopl.io/ch12/display.go
@@ -1,4 +1,4 @@
-package display
+package ch12
 
 import (
 	"fmt"
@@ -19,9 +19,29 @@ func display(path string, v reflect.Value) {
 			fieldPath := fmt.Sprintf("%s.%s", path, v.Type().Field(i).Name)
 			display(fieldPath, v.Field(i))
 		}
+	case reflect.Struct:
+		for i := 0; i < v.NumField(); i++ {
+			fieldPath := fmt.Sprintf("%s.%s", path, v.Type().Field(i).Name)
+			display(fieldPath, v.Field(i))
+		}
 	case reflect.Map:
 		for _, key := range v.MapKeys() {
-			display(fmt.Sprintf("%s[%s]", path, ))
+			display(fmt.Sprintf("%s[%s]", path, formatAtom(key)), v.MapIndex(key))
 		}
+	case reflect.Ptr:
+		if v.IsNil() {
+			fmt.Printf("%s = nil\n", path)
+		} else {
+			display(fmt.Sprintf("(*%s)", path), v.Elem())
+		}
+	case reflect.Interface:
+		if v.IsNil() {
+			fmt.Printf("%s = nil\n", path)
+		} else {
+			fmt.Printf("%s.type = %s\n", path, v.Elem().Type())
+			display(path+".value", v.Elem())
+		}
+	default:
+		fmt.Printf("%s = %s\n", path, formatAtom(v))
 	}
 }
--- gopl.io/main.go
+++ gopl.io/main.go
@@ -1,1 +1,44 @@
-package gopl_io
+package main
+
+import (
+	"flag"
+	"fmt"
+	"time"
+
+	"github.com/swanwish/go-common/logs"
+	"github.com/swanwish/go-study/gopl.io/ch12"
+)
+
+var op string
+
+func main() {
+	flag.StringVar(&op, "op", "display", "The operation to execute")
+	flag.Parse()
+	switch op {
+	case "display":
+		testDisplay()
+	case "format":
+		testFormat()
+	default:
+		logs.Errorf("Unknown operation")
+	}
+}
+
+func testFormat() {
+	var x int64 = 1
+	var d time.Duration = 1 * time.Nanosecond
+	fmt.Println(ch12.Any(x))
+	fmt.Println(ch12.Any(d))
+	fmt.Println(ch12.Any([]int64{x}))
+	fmt.Println(ch12.Any([]time.Duration{d}))
+}
+
+func testDisplay() {
+	var node struct {
+		Name   string
+		Length int64
+	}
+	node.Name = "Stephen"
+	node.Length = 100
+	ch12.Display("node", node)
+}
