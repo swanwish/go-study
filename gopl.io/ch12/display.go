035fc91c3428f6d90ec3d8b52d1e83b0cbe03bcf
Stephen <liwp.stephen@gmail.com>
2016-03-03 08:55:13 +0800

    add display and format test code

---
+++ gopl.io/ch12/display.go
@@ -0,0 +1,27 @@
+package display
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func Display(name string, x interface{}) {
+	fmt.Printf("Display %s %T:\n", name, x)
+	display(name, reflect.ValueOf(x))
+}
+
+func display(path string, v reflect.Value) {
+	switch v.Kind() {
+	case reflect.Invalid:
+		fmt.Printf("%s = invalid\n", path)
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < v.Len(); i++ {
+			fieldPath := fmt.Sprintf("%s.%s", path, v.Type().Field(i).Name)
+			display(fieldPath, v.Field(i))
+		}
+	case reflect.Map:
+		for _, key := range v.MapKeys() {
+			display(fmt.Sprintf("%s[%s]", path, ))
+		}
+	}
+}
